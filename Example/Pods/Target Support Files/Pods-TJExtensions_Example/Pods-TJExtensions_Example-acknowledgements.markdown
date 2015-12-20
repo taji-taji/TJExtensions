27368bd92b901eb9773bcb7a5154ced395286f28
Yutaka Tajika <y.tajika529@gmail.com>
2015-12-20 23:32:47 +0900

    Add examples

--- Example/Pods/Target Support Files/Pods-TJExtensions_Example/Pods-TJExtensions_Example-acknowledgements.markdown
+++ Example/Pods/Target Support Files/Pods-TJExtensions_Example/Pods-TJExtensions_Example-acknowledgements.markdown
@@ -3,7 +3,7 @@ This application makes use of the following third party libraries:
 
 ## TJExtensions
 
-Copyright (c) 2015 Yutaka Tajika <y.tajika529@gmail.com>
+Copyright (c) 2015 Yutaka Tajika
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- Example/TJExtensions/ViewController.swift
+++ Example/TJExtensions/ViewController.swift
@@ -10,15 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var borderedView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // ビューの背景色を指定
+        borderedView.backgroundColor = UIColor(hex: 0x859495, alpha: 1.0)
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        // 線の色をUIColorのExtensionで指定
+        let borderColor = UIColor(intRed: 255, green: 30, blue: 20, alpha: 1.0)
+        
+        // TopとRightに線をつける
+        borderedView.border([.Top, .Right], borderWidth: 3.5, borderColor: borderColor)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func textFieldDidBeginEditing(textField: UITextField){
+        print("textFieldDidBeginEditing:" + textField.text!)
+    }
+    
+    func textFieldShouldEndEditing(textField: UITextField) -> Bool {
+        print("textFieldShouldEndEditing:" + textField.text!)
+        
+        return true
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+
 
 }
 
