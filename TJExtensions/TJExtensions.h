70908303db695c36f72cab7d051f0696df6194f4
Yutaka Tajika <y.tajika529@gmail.com>
2016-11-11 19:56:33 +0900

    add header file

---
+++ TJExtensions/TJExtensions.h
@@ -0,0 +1,19 @@
+//
+//  TJExtensions.h
+//  TJExtensions
+//
+//  Created by tajika on 2016/11/11.
+//  Copyright © 2016年 Tajika. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TJExtensions.
+FOUNDATION_EXPORT double TJExtensionssVersionNumber;
+
+//! Project version string for TJExtensions.
+FOUNDATION_EXPORT const unsigned char TJExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TJExtensions/PublicHeader.h>
+
+
